1afbb8a24e97880a76e1bffc00ab33b9d49582a9
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-09-23 00:05:59 +0300

    Invoice Creator Authorization Handler

---
+++ IdentityNew/Authorization/InvoiceCreatorAuthorizationHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using IdentityNew.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace IdentityNew.Authorization
+{
+    public class InvoiceCreatorAuthorizationHandler :
+        AuthorizationHandler<OperationAuthorizationRequirement, Invoice >
+    {
+        UserManager<IdentityUser> _userManager;
+
+        public InvoiceCreatorAuthorizationHandler(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context, 
+            OperationAuthorizationRequirement requirement,
+            Invoice invoice)
+        {
+          if(context.User == null || invoice == null)
+                return Task.CompletedTask; 
+          
+          if(requirement.Name != Constans.CreateOperationName &&
+                requirement.Name != Constans.ReadOperationName && 
+                requirement.Name != Constans.UpdateOperationName &&
+                requirement.Name != Constans.DeleteOperationName)
+          {
+                return Task.CompletedTask;
+          }
+
+            if (invoice.CreatorId == _userManager.GetUserId(context.User))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
