13aaf9b03cff82d59ee1a4975101d5f8bd2d4228
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-09-22 23:18:13 +0300

    Authorization Operation Requirements

---
+++ IdentityNew/Authorization/InvoiceOperations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace IdentityNew.Authorization
+{
+    public class InvoiceOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+            new OperationAuthorizationRequirement { Name = Constans.CreateOperationName };
+        public static OperationAuthorizationRequirement Read =
+            new OperationAuthorizationRequirement { Name = Constans.ReadOperationName };
+        public static OperationAuthorizationRequirement Update =
+            new OperationAuthorizationRequirement { Name = Constans.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete =
+            new OperationAuthorizationRequirement { Name = Constans.DeleteOperationName };
+        public static OperationAuthorizationRequirement Approved =
+            new OperationAuthorizationRequirement { Name = Constans.ApprovedOperationName };
+        public static OperationAuthorizationRequirement Reject =
+            new OperationAuthorizationRequirement { Name = Constans.RejectOperationName };
+    }
+
+    public class Constans
+    {
+        public static readonly string CreateOperationName = "Create";
+        public static readonly string ReadOperationName = "Read"; 
+        public static readonly string UpdateOperationName = "Update";
+        public static readonly string DeleteOperationName = "Delete";
+        
+        
+        public static readonly string ApprovedOperationName = "Approved";
+        public static readonly string RejectOperationName = "Reject";
+    }
+}
